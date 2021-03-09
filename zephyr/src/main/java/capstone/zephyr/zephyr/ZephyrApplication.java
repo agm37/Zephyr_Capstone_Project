d89a07353f9f8db7570af535de6558033cb3b04a
Kyu <taw42@students.uwf.edu>
2021-03-08 20:44:34 -0600

    Generated Project Files

---
+++ zephyr/src/main/java/capstone/zephyr/zephyr/ZephyrApplication.java
@@ -0,0 +1,13 @@
+package capstone.zephyr.zephyr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZephyrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZephyrApplication.class, args);
+	}
+
+}
