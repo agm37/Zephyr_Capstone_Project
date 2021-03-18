fe75331ed42a1e43e1d5a47f9fcd1de0dfc5d601
Kyu <taw42@students.uwf.edu>
2021-03-17 20:55:36 -0500

    ApplicationController class

---
+++ zephyr/src/main/java/capstone/zephyr/zephyr/ApplicationController.java
@@ -0,0 +1,14 @@
+package capstone.zephyr.zephyr;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class ApplicationController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
