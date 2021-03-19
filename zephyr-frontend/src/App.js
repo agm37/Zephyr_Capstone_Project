80e83cc1e15f0a6a02ba4416f3e79c9c114fa46a
Ryan Gonzalez <rymg19@gmail.com>
2021-03-18 20:33:25 -0500

    frontend: Reformat App.js with 4-space indentation

--- zephyr-frontend/src/App.js
+++ zephyr-frontend/src/App.js
@@ -4,49 +4,42 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import VotingPage from './VotingPage';
 
 function App() {
-  return (
-    <Router>
+    return (
+        <Router>
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
 
-    <div className="App">
-         
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        
-      
-        <a
-          className="App-link"
-          href="/votingpage"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          VotingPage
-        </a> 
+                    <a
+                        className="App-link"
+                        href="/votingpage"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        VotingPage
+                    </a>
 
-
-
-        <Route exact = {true} path = "/votingpage" component = {VotingPage} />
-
-        
-
-
-      </header>
-      
-    </div>
-    </Router>
-
-  );
+                    <Route
+                        exact={true}
+                        path="/votingpage"
+                        component={VotingPage}
+                    />
+                </header>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
