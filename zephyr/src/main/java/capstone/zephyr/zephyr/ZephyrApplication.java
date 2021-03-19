521f8bef4552e9795d83252e37abc8e2e9097017
Ryan Gonzalez <rymg19@gmail.com>
2021-03-18 20:33:05 -0500

    backend: Add CORS support

--- zephyr/src/main/java/capstone/zephyr/zephyr/ZephyrApplication.java
+++ zephyr/src/main/java/capstone/zephyr/zephyr/ZephyrApplication.java
@@ -1,11 +1,28 @@
 package capstone.zephyr.zephyr;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ZephyrApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ZephyrApplication.class, args);
+	}
+
+	@Value("${app.cors.allowed-origins}")
+	private String[] corsAllowedOrigins;
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins(corsAllowedOrigins);
+			}
+		};
 	}
 }
