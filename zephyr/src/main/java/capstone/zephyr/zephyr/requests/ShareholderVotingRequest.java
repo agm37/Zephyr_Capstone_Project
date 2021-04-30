8c57f487476a14f011736ccf209ebeb96dd14fda
Ryan Gonzalez <rymg19@gmail.com>
2021-04-29 23:43:18 -0500

    backend: Remove an unused field/getter

--- zephyr/src/main/java/capstone/zephyr/zephyr/requests/ShareholderVotingRequest.java
+++ zephyr/src/main/java/capstone/zephyr/zephyr/requests/ShareholderVotingRequest.java
@@ -1,17 +1,12 @@
 package capstone.zephyr.zephyr.requests;
 
 public class ShareholderVotingRequest {
-    private int shareholderID;
     private int pollID;
     private int parameterNum;
 
     public ShareholderVotingRequest(int pollID, int parameterNum) {
         this.pollID = pollID;
         this.parameterNum = parameterNum;
-    }
-
-    public int getShareholderID() {
-        return shareholderID;
     }
 
     public int getPollID() {
